c5edefa5741d25000296e0c224b79e7296165e3f
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-02 21:42:16 +0000

    Added game logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,31 @@
+require 'set'
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
-    @letters = ('A'..'Z').to_a
+    @letters = 10.times.map { ('A'..'Z').to_a.sample }
   end
 
   def score
-    raise
+    @answer = params[:answer]
+    @letters = params[:letters]
+    @letters_match = letters_match(@letters, @answer) #this returns true or false
+    @english_word = is_english_word(@answer)
+  end
+
+  private
+
+  def letters_match(letters, answer)
+    letters_array = letters.split(" ").to_set
+    answer_array = answer.upcase.chars.to_set
+    answer_array.subset?(letters_array)
+  end
+  
+  def is_english_word(answer)
+    url = "https://wagon-dictionary.herokuapp.com/#{answer}"
+    word_in_json_dictionary = open(url).read
+    word = JSON.parse(word_in_json_dictionary)
+    word.key?('error') ? false : true
   end
 end
